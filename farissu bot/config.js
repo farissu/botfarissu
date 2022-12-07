3671498943a2ba1c3ab856199f605b569311a10d
Farissu <76688302+farissu@users.noreply.github.com>
2022-12-07 01:11:34 -0800

    Add files via upload

---
+++ farissu bot/config.js
@@ -0,0 +1,52 @@
+/**
+   * Create By Dika Ardnt.
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapis.xyz': '9d30efca66',
+}
+
+// Other
+global.owner = ['6288298476340']
+global.ownernomer = '6288298476340'
+global.premium = ['6288298476340']
+global.packname = 'Farissu'
+global.author = 'Farissu'
+global.sessionName = 'Farissu'
+global.prefa = ['','!','.','#','&']
+global.sp = ''
+global.mess = {
+    success: 'Done',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    premime: 'Fitur Khusus Farissu',
+    owner: 'Fitur Khusus Farissu',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 25
+}
+global.thumb = fs.readFileSync('./image/zens.jpg')
+global.vaze = { url: 'https://telegra.ph/file/15209657f9d4f59c7ca1e.mp4' }
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
